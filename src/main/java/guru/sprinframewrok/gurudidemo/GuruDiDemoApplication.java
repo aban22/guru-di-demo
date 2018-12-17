0af3e51f7ccca490d06d122ac4987f107609c534
Abel Fernandez <abelfergodevelop@gmail.com>
2018-12-17 18:55:32 +0100

    Clase 34

---
+++ src/main/java/guru/sprinframewrok/gurudidemo/GuruDiDemoApplication.java
@@ -0,0 +1,14 @@
+package guru.sprinframewrok.gurudidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruDiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuruDiDemoApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/guru/sprinframewrok/gurudidemo/GuruDiDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package guru.sprinframewrok.gurudidemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuruDiDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
